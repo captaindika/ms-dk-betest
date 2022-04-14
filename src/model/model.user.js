53ef0e0b3db60c08f7f3c3900ed11ae4b89029d7
captaindika <dika.frandita@gmail.com>
2022-04-15 02:51:11 +0700

    fix model add required true

--- src/model/model.user.js
+++ src/model/model.user.js
@@ -5,18 +5,22 @@ const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userName: {
         type: String,
+        required: true,
         unique: true
     },
     accountNumber: {
         type: String,
+        required: true,
         unique: true
     },
     emailAddress: {
         type: String,
+        required: true,
         unique: true
     },
     identityNumber: {
         type: String,
+        required: true,
         unique: true
     } 
 });
