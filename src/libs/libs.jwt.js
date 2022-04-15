68d9219452d99ffc5c2d29355f823b57bea0a791
captaindika <dika.frandita@gmail.com>
2022-04-15 22:11:06 +0700

    fix jwt verify token, add authorize open-api

--- src/libs/libs.jwt.js
+++ src/libs/libs.jwt.js
@@ -3,18 +3,21 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     try {
         // check header or url parameters or post parameters for token
+        if (!req.headers['authorization']) throw {
+            status: 401, message: 'Access token is missing or invalid'
+        }
         const token = req.headers['authorization'].split(' ')[1];
-        if (!token) throw {status: 403, message: 'No token provided'}
+        if (!token) throw { status: 401, message: 'Access token is missing or invalid' }
 
         // verifies secret and checks exp
-        jwt.verify(token, process.env.TOKEN_KEY, function(err, decoded) {      
-        if (err) throw {status: 500, message: 'Failed to authenticate token'}
-        // if everything is good, save to request for use in other routes
-        req.userId = decoded.id;
-        next();
+        jwt.verify(token, process.env.TOKEN_KEY, function (err, decoded) {
+            if (err) throw { status: 401, message: 'Failed to authenticate token' }
+            // if everything is good, save to request for use in other routes
+            req.userId = decoded.id;
+            next();
         });
     } catch (err) {
-        res.status(err.status || 500).json({status_code: err.status || 500, message: err.message})
+        res.status(err.status || 500).json({ status_code: err.status || 500, message: err.message })
     }
 
 }
@@ -22,5 +25,4 @@ const verifyToken = (req, res, next) => {
 module.exports = {
     verifyToken
 }
-  
-  
+
